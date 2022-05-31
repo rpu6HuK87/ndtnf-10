873c4fee07f83db9e55408b9baee07fe81025460
Heckfy <rr.akhmetoff@gmail.com>
2022-05-31 19:33:18 +0000

    Подключил Mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from'@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BooksModule } from './books/books.module';
 
+
 @Module({
-  imports: [BooksModule],
+  imports: [
+		ConfigModule.forRoot(),
+		MongooseModule.forRoot(process.env.DB_URL, {
+			user: process.env.DB_USERNAME,
+			pass: process.env.DB_PASSWORD,
+			dbName: process.env.DB_NAME,
+			useNewUrlParser: true,
+			useUnifiedTopology: true
+		}),
+		BooksModule
+	],
   controllers: [AppController],
   providers: [AppService],
 })
